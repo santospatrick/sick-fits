67b1116cc61a5c88e3231f088f9b4c1683125bf5
Patrick Santos <patrickspteixeira@gmail.com>
2021-02-14 15:06:43 -0300

    feat: global page loading feedback

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Page from 'components/Page';
+import Router from 'next/router';
+
+import NProgress from 'nprogress';
+import 'components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function _app({ Component, pageProps }) {
   return (
