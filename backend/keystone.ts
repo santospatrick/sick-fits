7379926badd0c3d4e88297d4de6bf7167b71485a
Patrick Santos <patrickspteixeira@gmail.com>
2021-02-23 21:37:55 -0300

    feat: add product and product image schemes

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,8 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -40,6 +42,8 @@ export default withAuth(
     },
     lists: createSchema({
       User,
+      Product,
+      ProductImage,
     }),
     ui: {
       // Show the UI only for people who pass this test
